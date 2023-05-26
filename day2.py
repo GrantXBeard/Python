a1f30aa14d5ce7304efc49152de130a32adba539
Grant Beard <Grantxbeard@gmail.com>
2023-05-26 15:45:20 -0600

    day2

---
+++ day2.py
@@ -0,0 +1,10 @@
+print('Hello!')
+name = input('name >')
+fav_food = input('favorite food >')
+location = input('current location >')
+fav_music = input('favorite music >')
+print('''Awesome!
+thanks '''+name+
+      ''' I also love '''+
+      fav_food+''' & '''+fav_music+'''!
+      You are soooo kool''')
