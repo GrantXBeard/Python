951500fc1d5166994752f64f2c54ec8c8c632e47
Grant Beard <Grantxbeard@gmail.com>
2023-05-26 16:10:41 -0600

    day6

---
+++ day6.py
@@ -0,0 +1,9 @@
+print('secure login')
+username = input("username > ")
+password = input("password > ")
+if username == 'Grant' and password == 'nipple':
+  print("howdy")
+elif username == 'Oscar' and password == 'baby':
+  print('Hi there')
+else:
+  print("bye bye")
