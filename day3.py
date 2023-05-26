1e89bacb88d938eb896d3f9c988bc9f1c35e463c
Grant Beard <Grantxbeard@gmail.com>
2023-05-26 16:02:02 -0600

    day3

---
+++ day3.py
@@ -0,0 +1,30 @@
+# myName = input("What's your name? ")
+# myLunch = input("What are you having for lunch? ")
+# print(myName, "is going to be chowing down on", myLunch, "very soon!")
+
+# number = input("Give me a number: ")
+# group = input("Give me a collective noun for a group of things: ")
+# thing = input("Give me the name of a weird or wacky thing: ")
+# print("No I don't think that", number, "is a", group, "of", thing,". That's just odd.")
+
+# print("=== Your Song Generator ===")
+# print("""You'll be asked a bunch of questions
+# then we'll make you up an amazing
+# song, totally copyright free 😭""")
+# print()
+# person = input("Name a person famous for something good: ")
+# thing = input("Name a thing they did: ")
+# place = input("Name a place you like: ")
+# rhyme = input("Give me a verb that rhymes with your person's name: ")
+# print()
+# print("There was a person called", person)
+# print("Who did something cool like", thing, "at the wonderful", place, "where you'll find me", rhyme)
+
+print('The Ultimate Wacky Recipe Maker')
+print('please input the following...')
+food = input('Name a food item > ')
+plant = input('Name a plant > ')
+method = input('Method of cooking > ')
+burned = input('Word to describe burned food > ')
+item = input('Household item > ')
+print(method, food, 'with', burned, plant, 'on a bed of', item)
