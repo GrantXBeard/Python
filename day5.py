3db9c6e76749cb525fd692fc64002cc66c37021a
Grant Beard <Grantxbeard@gmail.com>
2023-05-26 16:08:06 -0600

    day5

---
+++ day5.py
@@ -0,0 +1,10 @@
+My_Name = input("\033[34mWhat's your name?\n\033[0m")
+if My_Name == "Grant":
+  print("Howdy")
+
+myName = input("What's your name?: ")
+if myName == "David":
+ print("Welcome Dude!")
+ print("You're just the baldest dude I've ever seen")
+else:
+ print("Who on earth are you?!")
