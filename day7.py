d1557c2300392a25f2dfd517acd582e09dcf45bd
Grant Beard <Grantxbeard@gmail.com>
2023-05-27 16:22:06 -0600

    day7

---
+++ day7.py
@@ -0,0 +1,21 @@
+tvShow = input("what is your favorite fruit > ")
+if tvShow == "pineapple" :
+  hadSoda = input("have you ever had a pineapple soda? > ")
+  if hadSoda == "yes" :
+    print("it's sooooo good right!?")
+  else :
+    print("you should try it!")
+elif tvShow == "kiwi" :
+  hadSoda = input("have you ever had a kiwi soda? > ")
+  if hadSoda == "yes" :
+    print("it's sooooo good right!?")
+  else :
+    print("you should try it!")
+# elif tvShow == "banana" :
+  
+# elif tvShow == "coconut" :
+  
+# elif tvShow == "apple" :
+  
+else :
+  print("you should try some more fruits!")
