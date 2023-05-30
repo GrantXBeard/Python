dd599c57414a30dd7d4b5e62555b4d7b98c7e616
Grant Beard <Grantxbeard@gmail.com>
2023-05-29 23:29:06 -0600

    day8

---
+++ day8.py
@@ -0,0 +1,34 @@
+name = input("\033[34mwhat is your name? > \n\033[0m")
+if name == 'grant' or name == 'Grant' : 
+  print("hello " + name + "!")
+  mood = input("\033[34mon a scale of 1-3 how are you feeling today? > \n\033[0m")
+  if mood == '1' :
+    print("i hope it gets better")
+  elif mood == '2' :
+    print("could be worse right?")
+  elif mood == '3' :
+    print("hell yea " + name)
+  else :
+    print('well that doesnt make sense')
+if name == 'oscar' or name == 'Oscar' : 
+  print("hello " + name + "!")
+  mood = input("\033[34mon a scale of 1-3 how are you feeling today? > \n\033[0m")
+  if mood == '1' :
+    print("i hope it gets better")
+  elif mood == '2' :
+    print("could be worse right?")
+  elif mood == '3' :
+    print("hell yea " + name)
+  else :
+    print('well that doesnt make sense')
+if name == 'kristin' or name == 'Kristin' : 
+  print("hello " + name + "!")
+  mood = input("\033[34mon a scale of 1-3 how are you feeling today? > \n\033[0m")
+  if mood == '1' :
+    print("i hope it gets better")
+  elif mood == '2' :
+    print("could be worse right?")
+  elif mood == '3' :
+    print("hell yea " + name)
+  else :
+    print('well that doesnt make sense')
