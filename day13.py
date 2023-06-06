c0cdca2dc903328d6be7ecf03975848d05ead968
Grant Beard <Grantxbeard@gmail.com>
2023-06-06 17:34:41 -0600

    add all percent points

--- day13.py
+++ day13.py
@@ -7,11 +7,20 @@ print()
 score = float(input('Your score: '))
 per = round((score / max)*100, 2)
 print(per)
-if per == 90 :
-  print('You got a',str(per) + '%', 'which is a A+' )
+if per >= 90 :
+  print('You got a',str(per) + '%', 'on the', name, 'exam. Your grade is an A+')
+elif per < 90 and per >= 80 :
+  print('You got a',str(per) + '%', 'on the', name, 'exam. Your grade is an A-')
+elif per < 80 and per >= 70 :
+  print('You got a',str(per) + '%', 'on the', name, 'exam. Your grade is an B')
+elif per < 70 and per >= 60 :
+  print('You got a',str(per) + '%', 'on the', name, 'exam. Your grade is an C')
+elif per < 60 and per >= 50 :
+  print('You got a',str(per) + '%', 'on the', name, 'exam. Your grade is an D')
+elif per < 50 :
+  print('You got a',str(per) + '%', 'on the', name, 'exam. Your grade is an U')
 else :
   print('try again')
-
 
 
 # 90 = a+
