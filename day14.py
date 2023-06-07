8ae13c3e71136bf0a0eb1f04ada4b61692c2f084
Grant Beard <Grantxbeard@gmail.com>
2023-06-07 15:54:35 -0600

    add initial setup

---
+++ day14.py
@@ -0,0 +1,10 @@
+from getpass import getpass as input
+
+print('E P I C    🪨  📄 ✂️    B A T T L E')
+print()
+print('Select your move (R, P or S)')
+print()
+player1 = input('Player 1 chooses > ')
+print(player1)
+player2 = input('Player 2 chooses > ')
+print(player2)
