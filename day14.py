fb22230a270d2676f66bbf4f28275595fdb0de07
Grant <grantxbeard@gmail.com>
2023-08-05 07:33:03 -0600

    day14

--- day14.py
+++ day14.py
@@ -5,6 +5,36 @@ print()
 print('Select your move (R, P or S)')
 print()
 player1 = input('Player 1 chooses > ')
-print(player1)
 player2 = input('Player 2 chooses > ')
-print(player2)
+
+if player1 == 'R' :
+  if player2 == 'S' :
+    print('🪨  beats ✂️  Player 1 wins!')
+  elif player2 == 'P' :
+    print('📄  beats 🪨  Player 2 wins!')
+  elif player2 == 'R' :
+    print('Draw!')
+  else :
+    print('Invalid move Player 2')
+elif player1 == 'P' :
+  if player2 == 'S' :
+    print('✂️  beats 📄  Player 2 wins!')
+  elif player2 == 'P' :
+    print('Draw!')
+  elif player2 == 'R' :
+    print('📄  beats 🪨  Player 1 wins!')
+  else : 
+    print('Invalid move Player 2')
+elif player1 == 'S' : 
+  if player2 == 'S' :
+    print('Draw!')
+  elif player2 == 'P' : 
+    print('✂️  beats 📄  Player 1 wins!')
+  elif player2 == 'R' :
+    print('🪨  beats ✂️  Player 2 wins!')
+  else : 
+    print('Invalid move Player 2')
+else :
+  print('Invalid move Player 1')
+
+  
